37db0581ab973cd15c825fe41e2f103ba51e4133
taysonandtedi <99391664+taysonandtedi@users.noreply.github.com>
2022-03-07 09:53:40 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,12 @@ let distancia = 0
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
     distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
-    if (distancia < 9) {
+    if (distancia < 15) {
         basic.showString("" + (distancia))
+        if (distancia < 6) {
+            basic.showString("" + (distancia))
+            maqueen.motorStop(maqueen.Motors.All)
+        }
     } else {
         basic.showLeds(`
             # . # . #
@@ -13,4 +17,5 @@ basic.forever(function () {
             # . # . #
             `)
     }
+    maqueen.motorStop(maqueen.Motors.All)
 })
