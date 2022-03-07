4347f03b3946cbb15c20ff1df5eb8826197f4501
taysonandtedi <99391664+taysonandtedi@users.noreply.github.com>
2022-03-07 09:41:14 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+let distancia = 0
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+    distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (distancia < 9) {
+        basic.showString("" + (distancia))
+    } else {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+})
